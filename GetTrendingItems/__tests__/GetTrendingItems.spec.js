04e449aef693a87d7dad2a3988118d8eec94ab35
billy-reilly <billy_reilly@hotmail.co.uk>
2019-11-09 16:21:44 +0000

    add failing test to ensure not mergeable

--- GetTrendingItems/__tests__/GetTrendingItems.spec.js
+++ GetTrendingItems/__tests__/GetTrendingItems.spec.js
@@ -1,5 +1,9 @@
-describe("my first test", () => {
+describe("tests", () => {
   it("should pass", () => {
     expect(true).toBe(true);
+  });
+
+  it("should fail", () => {
+    expect(true).toBe(false);
   });
 });
